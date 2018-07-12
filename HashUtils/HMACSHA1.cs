12c949490b2f7941a433258760462b5bee7164c7
Andrés <forestrf@gmail.com>
2018-07-13 00:10:24 +0200

    Exposed more methods and renamed to better match what they do

--- HashUtils/HMACSHA1.cs
+++ HashUtils/HMACSHA1.cs
@@ -13,15 +13,28 @@ namespace HashUtils {
 		[ThreadStatic]
 		private static uint[] tmp4;
 
-		public static void ComputeHash(string key, ByteBuffer data, byte[] dst, int dstOffset) {
+		public static void ComputeHmacSha1(string key, ByteBuffer data, byte[] dst, int dstOffset) {
 			if (null == keyBytes) keyBytes = new byte[KEY_BYTE_LENGTH];
 			keyLength = Encoding.UTF8.GetBytes(key, 0, key.Length, keyBytes, 0);
-			ComputeHash(new ByteBuffer(keyBytes, 0, keyLength), data, dst, dstOffset);
+			ComputeHmacSha1(new ByteBuffer(keyBytes, 0, keyLength), data, dst, dstOffset);
 		}
 
-
-		public static void ComputeHash(ByteBuffer key, ByteBuffer data, byte[] dst, int dstOffset) {
+		public static void ComputeHmacSha1(ByteBuffer key, ByteBuffer data, byte[] dst, int dstOffset) {
 			SHA.ComputeHMAC_SHA1(key, data, new ByteBuffer(dst, dstOffset, 20), ref tmp1, ref tmp2, ref tmp3, ref tmp4);
+		}
+
+		public static void ComputeHmacSha256(string key, ByteBuffer data, byte[] dst, int dstOffset) {
+			if (null == keyBytes) keyBytes = new byte[KEY_BYTE_LENGTH];
+			keyLength = Encoding.UTF8.GetBytes(key, 0, key.Length, keyBytes, 0);
+			ComputeHmacSha256(new ByteBuffer(keyBytes, 0, keyLength), data, dst, dstOffset);
+		}
+
+		public static void ComputeHmacSha256(ByteBuffer key, ByteBuffer data, byte[] dst, int dstOffset) {
+			SHA.ComputeHMAC_SHA256(key, data, new ByteBuffer(dst, dstOffset, 32), ref tmp1, ref tmp2, ref tmp3, ref tmp4);
+		}
+
+		public static void ComputeSHA1(byte[] input, int inputLength, byte[] output20, int outputOffset) {
+			SHA.ComputeSHA1(input, inputLength, output20, outputOffset, ref tmp1, ref tmp4);
 		}
 
 		public static void ComputeSHA256(byte[] input, int inputLength, byte[] output32, int outputOffset) {
